ce98314e6b8590191fe44972597e32b45f4723db
John Sievertsen <john.sievertsen@ramseysolutions.com>
2022-07-12 16:28:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+}
+
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: 'ß',
+      url: 'https://en.wikipedia.org/wiki/%C3%9F',
+      author: 'I made this up ngl',
+      num_comments: 444,
+      points: 7,
+      objectID: 2,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = useStorageState('search', 'ß');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter(function (story) {
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Search Frameworks</h1>
+
+      <InputWithLabel id='search' label='Search' onInputChange={handleSearch} value={searchTerm} />
+
+      <hr />
+
+      <List list={searchedStories} />
     </div>
   );
 }
+
+const InputWithLabel = ({ id, label, value, onInputChange }) => (
+  <>
+    <label htmlFor={id}>{label}</label>
+    &nbsp;&nbsp;
+    <input id={id} type='text' value={value} onChange={onInputChange} />
+
+    <p>
+      Searching for <strong>{value}</strong>
+    </p>
+  </>
+);
+
+const Item = ({ item }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </li>
+);
+
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
 
 export default App;
